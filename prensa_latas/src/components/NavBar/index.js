6b5c0b44f68a94802060081fdbb7a9a7ee6d20aa
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-10-18 17:19:45 -0300

    created structure

---
+++ prensa_latas/src/components/NavBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './style.css';
+
+export default class NavBar extends React.Component {
+    render () {
+        return (
+            <div className='containner'>
+                <div className='bar'>
+                </div>
+                <div className='qr-button'>
+                </div>
+            </div>
+        )
+    }
+}
--- prensa_latas/src/index.js
+++ prensa_latas/src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact={true} component={App} />
-        </Switch>
-    </BrowserRouter>, document.getElementById('root'));
+    <NavBar/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
