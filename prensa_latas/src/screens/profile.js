f2bb26f10028017f05f09680aa137874a6fe1edd
Hiroshi <lucas.hh@hotmail.com>
2019-10-18 14:52:20 -0300

    add profile

---
+++ prensa_latas/src/screens/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function profile() {
+  return (
+    <div className="container-fluid">
+      <div className="Profile">
+          <QrReaderCam />
+      </div> 
+    </div>);  
+}
+
+export default Cam;
