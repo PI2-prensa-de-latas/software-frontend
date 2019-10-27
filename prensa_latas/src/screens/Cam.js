d2ece2f6076389352a078736c618b70b60030c1a
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-10-27 16:10:08 -0300

    fixing identing

--- prensa_latas/src/screens/Cam.js
+++ prensa_latas/src/screens/Cam.js
@@ -2,12 +2,12 @@ import React from 'react';
 import QrReaderCam from '../components/QrReader/index';
 
 function Cam() {
-  return (
-    <div className="container-fluid">
-      <div>
-          <QrReaderCam />
-      </div> 
-    </div>);  
+    return (
+        <div className="container-fluid">
+            <div>
+                <QrReaderCam />
+            </div>
+        </div>);
 }
 
 export default Cam;
