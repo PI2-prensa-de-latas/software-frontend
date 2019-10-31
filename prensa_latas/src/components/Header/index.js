6ec1ecfc47de8bcd74eab16c61ded7ecce675f68
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-10-31 12:38:55 -0300

    Created Header component
    I know that WaveHeader is so much stylish
    Congratulations to the designer who made it
    
    But I don't need all this style, actually,
    I need a button on header that calls a function

---
+++ prensa_latas/src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import styles from './style';
+
+import { FaChevronLeft } from 'react-icons/fa';
+
+class Header extends React.Component {
+    state = {  }
+    render() {
+        return (
+            <div style={styles.container}>
+                <div
+                    style={styles.icon}
+                    onClick={this.props.action}
+                >
+                    <FaChevronLeft/>
+                </div>
+                <div style={styles.title}>
+                    {this.props.title}
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Header;
