07e8d4d320c4dfe58a47f0936bec0923d4ee07ae
Hiroshi <lucas.hh@hotmail.com>
2019-11-01 02:43:22 -0300

    add a score placeholder for the moment

--- prensa_latas/src/components/ProfileScore/index.js
+++ prensa_latas/src/components/ProfileScore/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
+import {
+    GiSodaCan
+} from "react-icons/gi";
+
 import styles from './style'
+import './style.css';
+import headerImg from './../../assets/svg/arc.svg'
 
 export default class ProfileScore extends React.Component {
     render() {
         return (
             <>
                 <div style={styles.container} >
-                    <img style={styles.img}
-                        src={props.user.score}
-                    />
+                    <img style={styles.score}/>
                 </div>
-                <div style={styles.blankSpace} />
+                <div style={styles.circle}>
+                    <div style={styles.scoreFont}>
+                        480
+                        <GiSodaCan></GiSodaCan>
+                    </div>
+
+                </div>
+                
             </>
         )
     }
--- prensa_latas/src/components/ProfileScore/style.js
+++ prensa_latas/src/components/ProfileScore/style.js
@@ -5,9 +5,29 @@ const style = {
         width: '100%',
         heigth: 100     ,
         position: 'fixed',
-        top: -1,
-        left: 0,
+        top: "-1",
+        left: "0",
         textAlign: 'center',
+    },    
+    circle: {
+        height: "20vw",
+        width: "20vw",
+        backgroundColor: "#00695C",
+        borderRadius: "50%",
+        display: "inline-block",
+        position: 'fixed',
+        top: "20%",
+        left: "30%",
+        textAlign: 'center',
+    },
+    scoreFont: {
+        fontFamily: "roboto sans-serif",
+        fontStyle: "bold",
+        fontSize: "xx-large",
+        color: "#FFFFFF", 
+        textAlign: "center",
+        verticalAlign: "middle",
+        lineHeight: "20vw",
     }
 }
 
