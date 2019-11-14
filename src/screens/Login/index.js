bc460d981c22bc1c158ac2f20dce06425cfcd2a4
Hiroshi <lucas.hh@hotmail.com>
2019-11-14 01:57:48 -0300

    adding login header to screen

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Login from './../../components/Login';
-
+import LoginHeader from './../../components/LoginHeader';
 
 export default class LoginScreen extends React.Component {
 
     render() {
         return (
             <>
+                <LoginHeader/>
                 <Login/>
             </>
         )
