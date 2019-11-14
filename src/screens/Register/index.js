a1ef5c50b8f6135500bd5f4bb4c8fc78eb34e15d
Hiroshi <lucas.hh@hotmail.com>
2019-11-14 14:34:04 -0300

    add register screen

---
+++ src/screens/Register/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Register from './../../components/Register';
+
+export default class RegisterScreen extends React.Component {
+
+    render() {
+        return (
+            <>
+                <Register/>
+            </>
+        )
+    }
+}
