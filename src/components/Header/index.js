fd3c600f0e703c875a02d419186f6bfcc01bb009
Hiroshi <lucas.hh@hotmail.com>
2019-11-15 15:47:20 -0300

    add link to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
 import styles from './style';
-
 import { FaChevronLeft } from 'react-icons/fa';
+import {Link} from "react-router-dom";
 
 class Header extends React.Component {
 
     render() {
         return (
             <div style={styles.container}>
-                <div
-                    style={styles.icon}
-                    onClick={this.props.action}
-                >
-                    <FaChevronLeft/>
-                </div>
+                <Link to={this.props.link}>
+                    <div
+                        style={styles.icon}
+                    >
+                        <FaChevronLeft style={{fill:this.props.fill}}/>
+                    </div>
+                </Link>
+
                 <div style={styles.title}>
                     {this.props.title}
                 </div>
             </div>
-         );
+        );
     }
 }
  
