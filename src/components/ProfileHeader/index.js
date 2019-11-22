41ec2a58c7c63d0aeb4120321bf5d0ccc012f165
Hiroshi <lucas.hh@hotmail.com>
2019-11-22 15:25:46 -0300

    add test for profile pic if not undefined

--- src/components/ProfileHeader/index.js
+++ src/components/ProfileHeader/index.js
@@ -6,36 +6,45 @@ import {
 
 import axios from 'axios'
 import styles from './style'
+import defaultPic from './../../assets/images/Pic.png'
+
 
 const USER_TOKEN = localStorage.getItem('token');
+const USER_ID = localStorage.getItem('user');
 const AuthStr = 'Bearer '.concat(USER_TOKEN);
 const URL = 'http://localhost:1337/user';
 
 export default class ProfileHeader extends React.Component {
-    
+
     state = {
         user: ''
     }
-    
-    componentDidMount(){
-        axios
-            .get(URL,
+
+    componentDidMount = async () => {
+        console.log(USER_ID);
+        await axios
+            .get(`${URL}/${USER_ID}`,
                 {headers: {Authorization: AuthStr}})
-        .then(response => this.setState({user: response.data}))
+            .then(response => this.setState({user: response.data}));
+        console.log(this.state.user.pic);
+
+        if (this.state.user.pic === undefined) {
+            this.setState({user: {...this.state.user, pic: defaultPic}})
+        };
     }
-    
+
     render() {
         return (
             <>
-                <div style={styles.container} >
-                    <img style={styles.profile_pic_uri}
-                        src={this.state.user.pic}
+                <div style={styles.container}>
+                    <img style={styles.profilePic}
+                         src={this.state.user.pic}
                     />
                     <div style={styles.name}>
                         {this.state.user.name}
                         <MdEdit style={styles.editIcon}/>
                     </div>
-                    
+
                 </div>
             </>
         )
