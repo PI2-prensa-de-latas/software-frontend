a34df00a6d400d65aab014f5d258df00f3abf6fc
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-12-04 13:28:35 -0300

    changed default URL to production back-end

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-    baseURL: "http://localhost:1337",      // development
-    // baseURL: "http://159.65.236.163:8080/" // production
+    // baseURL: "http://localhost:1337",      // development
+    baseURL: "http://159.65.236.163:8080/" // production
 });
 
 api.interceptors.request.use(async config => {
